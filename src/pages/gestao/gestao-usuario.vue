007ce0bb19fcb72a02e881550c56d4275d553df1
Guilherme Carvalho <luisguilherme.carvalho@ucsal.edu.br>
2024-06-05 15:58:57 -0300

    Fazendo Crud para usuários

--- src/pages/gestao/gestao-usuario.vue
+++ src/pages/gestao/gestao-usuario.vue
@@ -1,21 +1,25 @@
 <script setup>
-
-import { ref } from 'vue';
-import { FilterMatchMode, FilterOperator } from "primevue/api";
 import { useToast } from "primevue/usetoast";
-// import { useSupabaseClient } from '@supabase/vue';
 
 // * Definindo variáveis
 const supabase = useSupabaseClient();
+const filters = ref({ global: { value: null } });
 const toast = useToast();
 const showDialog = ref(false);
 const submitted = ref(false);
+const selectedUser = ref(null);
+const deleteUserDialog = ref(false);
+const deleteUsersDialog = ref(false);
 
-const cadastrarNome = ref('');
-const cadastrarEmail = ref('');
-const cadastrarSenha = ref('');
-const cadastrarConfirmarSenha = ref('');
-const cadastrarCargo = ref('');
+const cadastrarNome = ref("");
+const cadastrarEmail = ref("");
+const cadastrarSenha = ref("");
+const cadastrarConfirmarSenha = ref("");
+const cadastrarCargo = ref("");
+
+const users = ref([]);
+const selectedUsers = ref([]);
+
 
 function openNew() {
   showDialog.value = true; // Abre o diálogo
@@ -24,52 +28,162 @@ function openNew() {
 function hideDialog() {
   showDialog.value = false; // Fecha o diálogo
   submitted.value = false; // Reseta o estado enviado
-  // Limpa os campos
-  cadastrarNome.value = '';
-  cadastrarEmail.value = '';
-  cadastrarSenha.value = '';
-  cadastrarConfirmarSenha.value = '';
-  cadastrarCargo.value = '';
+  cadastrarNome.value = "";
+  cadastrarEmail.value = "";
+  cadastrarSenha.value = "";
+  cadastrarConfirmarSenha.value = "";
+  cadastrarCargo.value = "";
 }
 
 async function saveUser() {
   submitted.value = true;
-  
+
   if (cadastrarSenha.value !== cadastrarConfirmarSenha.value) {
-    toast.add({ severity: 'error', summary: 'Erro', detail: 'As senhas não coincidem' });
+    toast.add({
+      severity: "error",
+      summary: "Erro",
+      detail: "As senhas não coincidem",
+    });
     return;
   }
-  
-  const cargo = cadastrarCargo.value === 'Administrador' ? 'Administrador' : 'Funcionário';
-  
-  try {
+
+  const cargo =
+    cadastrarCargo.value === "Administrador" ? "Administrador" : "Funcionário";
+
+    try {
     const { data, error } = await supabase.auth.signUp({
       email: cadastrarEmail.value,
       password: cadastrarSenha.value,
       options: {
         data: {
           nome: cadastrarNome.value,
-          cargo: cargo // Definindo o valor do cargo
-        }
-      }
+          cargo: cadastrarCargo.value,
+        },
+      },
     });
 
     if (error) {
       throw error;
     }
 
-    console.log('Usuário salvo:', data);
+    console.log("Usuário salvo:", data);
     showDialog.value = false; // Fecha o diálogo após salvar
     hideDialog();
-    toast.add({ severity: 'success', summary: 'Sucesso', detail: 'Usuário cadastrado com sucesso' });
+    toast.add({
+      severity: "success",
+      summary: "Sucesso",
+      detail: "Usuário cadastrado com sucesso",
+    });
+    fetchUsers();
   } catch (error) {
-    console.error('Erro ao salvar usuário:', error.message);
-    toast.add({ severity: 'error', summary: 'Erro', detail: 'Erro ao cadastrar usuário' });
+    console.error("Erro ao salvar usuário:", error.message);
+    toast.add({
+      severity: "error",
+      summary: "Erro",
+      detail: "Erro ao cadastrar usuário",
+    });
+  }
+}
+
+
+async function fetchUsers() {
+  try {
+    const { data, error } = await supabase.from('users').select('*');
+
+    if (error) {
+      throw error;
+    }
+
+    users.value = data.map(user => ({
+      ...user,
+      nome: user.nome || "",
+      email: user.email || "",
+      cargo: user.cargo || "",
+    }));
+
+    console.log("USUÁRIOS = ", users.value);
+  } catch (error) {
+    console.error('Erro ao buscar usuários:', error.message);
+  }
+}
+
+async function deleteUser() {
+  try {
+    const idToDelete = selectedUser.value.id;
+    const { error } = await supabase.from('users').delete().eq('id', idToDelete);
+
+    if (error) {
+      throw error;
+    }
+
+    // Exibir mensagem de sucesso
+    toast.add({
+      severity: "success",
+      summary: "Sucesso",
+      detail: "Usuário excluído com sucesso",
+    });
+  } catch (error) {
+    console.error("Erro ao excluir usuário:", error.message);
+    // Exibir mensagem de erro
+    toast.add({
+      severity: "error",
+      summary: "Erro",
+      detail: "Erro ao excluir usuário",
+    });
+  } finally {
+    // Fechar o diálogo de exclusão de usuário
+    deleteUserDialog.value = false;
+  }
+}
+
+
+async function deleteSelectedUsers() {
+  try {
+    const idsToDelete = selectedUsers.value.map(user => user.id);
+    const { error } = await supabase.from('users').delete().in('id', idsToDelete);
+
+
+    if (error) {
+      throw error;
+    }
+
+    // Exibir mensagem de sucesso
+    toast.add({
+      severity: "success",
+      summary: "Sucesso",
+      detail: "Usuários selecionados excluídos com sucesso",
+    });
+  } catch (error) {
+    console.error("Erro ao excluir usuários selecionados:", error.message);
+    // Exibir mensagem de erro
+    toast.add({
+      severity: "error",
+      summary: "Erro",
+      detail: "Erro ao excluir usuários selecionados",
+    });
+  } finally {
+    // Fechar o diálogo de exclusão de usuários
+    deleteUsersDialog.value = false;
   }
 }
 
 
 
+
+
+function confirmDeleteUser(user) {
+  selectedUser.value = user;
+  deleteUserDialog.value = true;
+}
+
+
+function confirmDeleteSelected() {
+  deleteUsersDialog.value = true;
+}
+
+onMounted(() => {
+  fetchUsers();
+});
 
 </script>
 
@@ -77,20 +191,92 @@ async function saveUser() {
   <div class="grid crud-demo">
     <div class="col-12">
       <div class="card">
-        <Toast />
         <Toolbar class="mb-4">
           <template #start>
             <div class="my-2">
               <h1></h1>
-              <Button label="Cadastrar Usuário" icon="pi pi-plus" class="p-button-success mr-2" @click="openNew" />
               <Button
-                label="Delete" icon="pi pi-trash" class="p-button-danger"
+                label="Cadastrar Usuário"
+                icon="pi pi-plus"
+                class="p-button-success mr-2"
+                @click="openNew"
+              />
+              <Button
+                label="Delete"
+                icon="pi pi-trash"
+                class="p-button-danger"
                 :disabled="!selectedUsers || !selectedUsers.length"
                 @click="confirmDeleteSelected"
               />
             </div>
           </template>
+          <template #end>
+            <Button
+              label="Export"
+              icon="pi pi-upload"
+              class="p-button-help"
+              @click="exportCSV($event)"
+            />
+          </template>
         </Toolbar>
+
+        <DataTable
+        ref="dt"
+        v-model:selection="selectedUsers"
+        :value="users"
+        data-key="id"
+        :paginator="true"
+        :rows="10"
+        :filters="filters"
+        paginator-template="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+        :rows-per-page-options="[5, 10, 25]"
+        current-page-report-template="Showing {first} to {last} of {totalRecords} users"
+        responsive-layout="scroll"
+      >
+        <template #header>
+          <div class="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
+            <h5 class="m-0">Gerenciar Usuário</h5>
+            <span class="block mt-2 md:mt-0 p-input-icon-left">
+              <i class="pi pi-search" />
+              <InputText v-model="filters.global.value" placeholder="Procurar" />
+            </span>
+          </div>
+        </template>
+
+        <Column field="nome" header="Nome" :sortable="true">
+          <template #body="slotProps">
+            <span class="p-column-title">Nome</span>
+            {{ slotProps.data.nome }}
+          </template>
+        </Column>
+        <Column field="email" header="Email" :sortable="true">
+          <template #body="slotProps">
+            <span class="p-column-title">Email</span>
+            {{ slotProps.data.email }}
+          </template>
+        </Column>
+        <Column field="cargo" header="Cargo" :sortable="true">
+          <template #body="slotProps">
+            <span class="p-column-title">Cargo</span>
+            {{ slotProps.data.cargo }}
+          </template>
+        </Column>
+        <Column selection-mode="multiple" header-style="width: 3em" />
+        <Column>
+          <template #body="slotProps">
+            <Button
+              icon="pi pi-pencil"
+              class="p-button-rounded p-button-success mr-2"
+              @click="editUser(slotProps.data)"
+            />
+            <Button
+              icon="pi pi-trash"
+              class="p-button-rounded p-button-warning"
+              @click="confirmDeleteUser(slotProps.data)"
+            />
+          </template>
+        </Column>
+      </DataTable>
       </div>
     </div>
   </div>
@@ -101,50 +287,110 @@ async function saveUser() {
     <template #default>
       <div class="p-field">
         <label for="name"><strong>Nome:</strong></label>
-        <InputText id="name" class="inputCSS" v-model="cadastrarNome" placeholder="Digite seu nome" />
+        <InputText
+          id="name"
+          class="inputCSS"
+          v-model="cadastrarNome"
+          placeholder="Digite seu nome"
+        />
       </div>
       <div class="p-field">
         <label for="email"><strong>Email:</strong></label>
-        <InputText id="email" class="inputCSS" v-model="cadastrarEmail" placeholder="Digite seu email" />
+        <InputText
+          id="email"
+          class="inputCSS"
+          v-model="cadastrarEmail"
+          placeholder="Digite seu email"
+        />
       </div>
       <div class="p-field">
         <label for="password"><strong>Senha:</strong></label>
-        <Password id="password" class="inputCSS" v-model="cadastrarSenha" placeholder="Digite sua senha" />
+        <Password
+          id="password"
+          class="inputCSS"
+          v-model="cadastrarSenha"
+          placeholder="Digite sua senha"
+        />
       </div>
       <div class="p-field">
         <label for="confirmPassword"><strong>Confirmar Senha:</strong></label>
-        <Password id="confirmPassword" class="inputCSS" v-model="cadastrarConfirmarSenha" placeholder="Digite novamente sua senha" />
+        <Password
+          id="confirmPassword"
+          class="inputCSS"
+          v-model="cadastrarConfirmarSenha"
+          placeholder="Digite novamente sua senha"
+        />
       </div>
-      <div class="field" style="margin-top: 20px;">
+      <div class="field" style="margin-top: 20px">
         <label class="mb-3"><strong>Cargo</strong></label>
         <div class="formgrid grid">
           <div class="field-radiobutton col-6">
-            <RadioButton id="cargo1" v-model="cadastrarCargo" name="cargo" value="Administrador" />
+            <RadioButton
+              id="cargo1"
+              v-model="cadastrarCargo"
+              name="cargo"
+              value="Administrador"
+            />
             <label for="cargo1">Administrador</label>
           </div>
           <div class="field-radiobutton col-6">
-            <RadioButton id="cargo2" v-model="cadastrarCargo" name="cargo" value="Funcionario" />
+            <RadioButton
+              id="cargo2"
+              v-model="cadastrarCargo"
+              name="cargo"
+              value="Funcionario"
+            />
             <label for="cargo2">Funcionário</label>
           </div>
         </div>
       </div>
     </template>
     <template #footer>
-      <Button label="Cancelar" icon="pi pi-times" class="p-button-text" @click="hideDialog" />
-      <Button label="Salvar" icon="pi pi-check" class="p-button-text" @click="saveUser" />
+      <Button
+        label="Cancelar"
+        icon="pi pi-times"
+        class="p-button-text"
+        @click="hideDialog"
+      />
+      <Button
+        label="Salvar"
+        icon="pi pi-check"
+        class="p-button-text"
+        @click="saveUser"
+      />
     </template>
   </Dialog>
+
+  <Dialog v-model:visible="deleteUserDialog" :style="{ width: '450px' }" header="Confirmar exclusão" :modal="true">
+    <div class="flex align-items-center justify-content-center">
+      <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
+      <span v-if="selectedUser">Tem certeza de que deseja excluir o usuário <b>{{ selectedUser.nome }}</b>?</span>
+    </div>
+    <template #footer>
+      <Button label="Não" icon="pi pi-times" class="p-button-text" @click="deleteUserDialog = false" />
+      <Button label="Sim" icon="pi pi-check" class="p-button-text" @click="deleteUser" />
+    </template>
+  </Dialog>
+  
+  <Dialog v-model:visible="deleteUsersDialog" :style="{ width: '450px' }" header="Confirmar exclusão" :modal="true">
+    <div class="flex align-items-center justify-content-center">
+      <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
+      <span v-if="selectedUsers && selectedUsers.length > 0">Tem certeza de que deseja excluir os usuários selecionados?</span>
+    </div>
+    <template #footer>
+      <Button label="Não" icon="pi pi-times" class="p-button-text" @click="deleteUsersDialog = false" />
+      <Button label="Sim" icon="pi pi-check" class="p-button-text" @click="deleteSelectedUsers" />
+    </template>
+  </Dialog>
+  
 </template>
 
-
-
 <style scoped>
-
 .crud-demo .p-toolbar {
   justify-content: space-between;
 }
 
-.InputCSS{
+.InputCSS {
   margin-left: 10px;
 }
 
@@ -152,5 +398,4 @@ async function saveUser() {
   margin-bottom: 20px;
   margin-right: 10px;
 }
-
 </style>
